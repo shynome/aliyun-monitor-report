e2ed0d8c9a1051137f1fa66e536e1b976c203577
shynome <shynome@gmail.com>
2019-07-31 17:33:32 +0800

    api 添加接口

--- api/server.go
+++ api/server.go
@@ -4,9 +4,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// Server api
+// Server all
 var Server = echo.New()
 
+// API group
+var API = Server.Group("/api")
+
+// Register group
 func Register(prefix string, m ...echo.MiddlewareFunc) *echo.Group {
-	return Server.Group(prefix, m...)
+	return API.Group(prefix, m...)
 }
