418060fd1fa694050ad604826c09d75ff509e302
shynome <shynome@gmail.com>
2019-08-12 11:25:48 +0800

    支持 unix 监听

--- cmd/aliyun-report/server.go
+++ cmd/aliyun-report/server.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"net"
+	"net/url"
+	"os"
+
 	"github.com/shynome/aliyun-monitor-report/api"
 	_ "github.com/shynome/aliyun-monitor-report/api/init"
 )
 
 func main() {
-	api.Server.Logger.Fatal(api.Server.Start(":3000"))
+	e := api.Server
+	endpoint := os.Getenv("LISTEN_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "http://:3000"
+	}
+	uri, err := url.Parse(endpoint)
+	if err != nil {
+		e.Logger.Fatal(err)
+		return
+	}
+	if uri.Scheme == "http" {
+		e.Logger.Fatal(e.Start(uri.Host))
+		return
+	}
+	if uri.Scheme == "unix" {
+		os.Remove(uri.Path)
+		l, err := net.Listen("unix", uri.Path)
+		if err != nil {
+			e.Logger.Fatal(err)
+			return
+		}
+		e.Listener = l
+		e.Logger.Fatal(e.Start(""))
+		return
+	}
+	e.Logger.Fatal("SERVER_ENDPOINT must be http://:3000 or unix:///tmp/foo.sock")
 }
